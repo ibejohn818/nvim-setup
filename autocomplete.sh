938bf3dc8e81977b757617d2b12991dede38f988
John Hardy <john.hardy@me.com>
2020-03-17 10:25:28 -0700

    update sub modules

--- autocomplete.sh
+++ autocomplete.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
-PWD=pwd
+HWD=$(pwd)
 
 echo "MAKE LIBS DIR"
+rm -rf ~/ycm_build
 cd ~
 mkdir ycm_build
 cd ycm_build
 
-echo "CMAKE GENERATOR"
-cmake -G "Unix Makefiles" . ${PWD}/plugins/youcompleteme/third_party/ycmd/cpp
+echo "CMAKE GENERATOR: ${HWD}"
+cmake -G "Unix Makefiles" . ${HWD}/plugins/youcompleteme/third_party/ycmd/cpp
 
-echo "CMAKE BUILD"
+echo "CMAKE BUILD: ${HWD}"
 cmake --build . --target ycm_core --config Release
 
 echo "BUILD GO LANG"
-cd ${PWD}/third_party/ycmd/third_party/go/src/golang.org/x/tools/cmd/gopls
+cd ${HWD}/third_party/ycmd/third_party/go/src/golang.org/x/tools/cmd/gopls
 go build
 
 echo "BUILD TYPE/JAVA SCRIPT SERVER"
-cd ${PWD}/third_party/ycmd
+cd ${HWD}/third_party/ycmd
 npm install -g --prefix third_party/tsserver typescript
 
 echo "BUILD PYTHON SERVER"
-cd ${PWD}/plugins/youcompleteme
-python install.py --go-completer --ts-completer 
+cd ${HWD}/plugins/youcompleteme
+python3.8 install.py --go-completer --ts-completer 
 
 echo "DONE"
 
